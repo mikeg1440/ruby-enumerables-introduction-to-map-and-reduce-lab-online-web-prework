0cf4985ed6360287d691fb9aa8d557b3316a52e0
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-26 17:19:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,6 +52,12 @@ end
 
 def reduce_to_all_true(source_array)
   
+  source_array.each do |element|
+    if !element
+      return false
+    end
+  end
+  true
 end
 
 def reduce_to_any_true(source_array)
